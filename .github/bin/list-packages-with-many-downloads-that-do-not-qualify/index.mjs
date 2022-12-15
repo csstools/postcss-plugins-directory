d0d695ac5e46ee89ae4631f9a225598168751be7
Romain Menke <romainmenke@gmail.com>
2022-12-15 22:16:58 +0100

    more experiments

---
+++ .github/bin/list-packages-with-many-downloads-that-do-not-qualify/index.mjs
@@ -0,0 +1,25 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const maintainedPlugins = new Set(JSON.parse(await fs.readFile('./npm-data/maintained-plugins.json')).objects.map((x) => x.package.name));
+
+const pluginsList = {
+	objects: [
+		...JSON.parse(await fs.readFile('./npm-data/plugins.json')).objects,
+		// ...JSON.parse(await fs.readFile('./npm-data/maybe-plugins.json')).objects,
+	]
+};
+for (let i = 0; i < pluginsList.objects.length; i++) {
+	const plugin = pluginsList.objects[i];
+	if (maintainedPlugins.has(plugin.package.name)) {
+		continue
+	}
+
+	const pluginFilePath = path.join('npm-data', 'plugins', plugin.package.name) + '.json'
+	const pluginData = JSON.parse(await fs.readFile(pluginFilePath))
+	if ('_downloads' in pluginData && pluginData._downloads < 1000_000) {
+		continue;
+	}
+
+	console.log(plugin.package.name, pluginData._downloads);
+}
